b0329c77b9cbcf997eaa9e4a92ef9aa93839615e
joebates25 <joebates25>
2014-10-01 15:58:50 -0500

    Events page readded
    Events page added back to repo (not sure why it was deleted)

--- events.php
+++ events.php
@@ -2,11 +2,11 @@
 <html>
 <!--Begin Code-->
 <head>
-    <title>GEAUXNSBE</title>
+	<title>GEAUXNSBE</title>
     <link rel="stylesheet" type="text/css" href="styles/main.css">
-    <link rel="stylesheet" type="text/css" href="styles/events.css">
+	<link rel="stylesheet" type="text/css" href="styles/events.css">
     <link rel="stylesheet" type="text/css" href="styles/navmenu.css"> 
-    <link rel="shortcut icon" type="img/ico" href="images/favicon.ico">
+	<link rel="shortcut icon" type="img/ico" href="images/favicon.ico">
         <link href='http://fonts.googleapis.com/css?family=Oswald:400,700,300' rel='stylesheet' type='text/css'>
 
     <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
@@ -20,7 +20,7 @@
 <div id="container">
 <?php include("header.php") ?>
     
-    <div id="menu">
+	<div id="menu">
         <ul>
             <?php
                 include("navmenu.php");
@@ -100,7 +100,7 @@
         ?>
         </div>
         
-    <!--<iframe src="https://www.google.com/calendar/embed?height=600&amp;wkst=1&amp;bgcolor=%23FFFFFF&amp;src=f38b27sght4m73mcml7isiq0t0%40group.calendar.google.com&amp;color=%232952A3&amp;ctz=America%2FChicago" style=" border-width:0 " width="800" height="600" frameborder="0" scrolling="no"></iframe> -->
+    <iframe src="https://www.google.com/calendar/embed?height=600&amp;wkst=1&amp;bgcolor=%23FFFFFF&amp;src=f38b27sght4m73mcml7isiq0t0%40group.calendar.google.com&amp;color=%232952A3&amp;ctz=America%2FChicago" style=" border-width:0 " width="800" height="600" frameborder="0" scrolling="no"></iframe> 
     
 </div>
     </body>
--- scripts/events.js
+++ scripts/events.js
@@ -7,24 +7,24 @@ $(document).ready(function(){
 	$('.start').find('.arrow').rotate(0, 'slow');
 	$('.head').click( function (e) {
 
-		if (!($(this).is('#headstylepast'))){
-			if ($(this).parent().hasClass('active')){
-				$(this).next().slideToggle();
-				$(this).parent().removeClass("active");
-				$(this).find('.arrow').rotate(90, 'slow');
-			}
-			else{
-				//if row is inactive
-				//close active row if exists
-				$(this).parent().parent().find('.active').find('.body').slideToggle();
-				$(this).parent().parent().find('.active').find('.arrow').rotate(90, 'slow');
-				$(this).parent().parent().find('.active').removeClass('active');
-				
-				//open row and make acive id
-				$(this).next().slideToggle();
-				$(this).find('.arrow').rotate(0, 'slow');
-				$(this).parent().addClass("active");
-			}
+	if (!($(this).is('#headstylepast'))){
+		if ($(this).parent().hasClass('active')){
+			$(this).next().slideToggle();
+			$(this).parent().removeClass("active");
+			$(this).find('.arrow').rotate(90, 'slow');
+		}
+		else{
+			//if row is inactive
+			//close active row if exists
+			$(this).parent().parent().find('.active').find('.body').slideToggle();
+			$(this).parent().parent().find('.active').find('.arrow').rotate(90, 'slow');
+			$(this).parent().parent().find('.active').removeClass('active');
+			
+			//open row and make acive id
+			$(this).next().slideToggle();
+			$(this).find('.arrow').rotate(0, 'slow');
+			$(this).parent().addClass("active");
+		}
 
 		}
 	});
